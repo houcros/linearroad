66400cf74ead865fc1a0c0b9a5470e7a037d3de4
Ignacio N. Lucero Ascencio <igwisnirrol@gmail.com>
2016-07-05 02:42:04 +0200

    Working on segment statistics.

--- src/main/scala/du/tu_berlin/dima/LinearRoad.scala
+++ src/main/scala/du/tu_berlin/dima/LinearRoad.scala
@@ -29,23 +29,7 @@ object LinearRoad {
     // Get custom data source
     val dataStream = new CarReportsSource[Array[Int]](inputFile)
     val reports: DataStream[Array[Int]] = env.addSource(dataStream)(TypeInformation.of(classOf[Array[Int]]))
-
-    // Data transformations
-    /*
-    val b: AllWindowedStream[Array[Int], TimeWindow] = reports.windowAll(TumblingEventTimeWindows.of(Time.seconds(3)))
-    b.apply( (timeWindow, iterable, collector: Collector[String]) => {
-      var out = "Window: " + String.valueOf(timeWindow.getStart) + "\n"
-      for (report <- iterable) {
-        for (reportItem <- report) {
-          out += reportItem + " "
-        }
-        out += "\n"
-      }
-      collector.collect(out)
-    })(TypeInformation.of(classOf[String]))
-      .print()
-    */
-
+    
     // **ACCIDENT DETECTION**
     val accidentStream: DataStream[Int] = reports.timeWindowAll(Time.seconds(4*30), Time.seconds(1*30)) // reports are every 30 seconds
       .apply( (timeWindow: TimeWindow, iterable, collector: Collector[Int]) => {
@@ -77,19 +61,41 @@ object LinearRoad {
 
 
     // **SEGMENT STATISTICS**
+
     // WARNING: fix, currently gives for this minute, and I want for last minute!
     // Number of vehicles during minute prior to current minute
     val numberOfVehicles: DataStream[Int] = reports.timeWindowAll(Time.minutes(1)) // in one minute we can have either 1 or 2 reports from each car
       .apply( (timeWindow: TimeWindow, iterable, collector: Collector[Int]) => {
 
-        val nov = iterable.groupBy(_(2)).size
-        collector.collect(nov)
+      val nov = iterable.groupBy(_(2)).size // group by VID and count different vehicles
+      collector.collect(nov)
 
     })(TypeInformation.of(classOf[Int]))
     //numberOfVehicles.print()
 
     // Average velocity
     // TODO: LAV
+    val latestAverageVelocity: DataStream[(Int, Int, Int, Int)] = reports.timeWindowAll(Time.minutes(5), Time.minutes(1))
+      .apply( (timeWindow: TimeWindow, iterable, collector: Collector[(Int, Int, Int, Int)]) => {
+
+        // Position := expressway, direction and segment
+        val positions = iterable.groupBy(rep => (rep(4): Int, rep(6): Int, rep(7): Int)) // group by expressway, direction and segment
+        positions.foreach( reportsAtPosition => { // for each position calculate avg speed
+
+          val grouppedReports = reportsAtPosition._2.groupBy(_(2)) // group as there can be more than one report per vehicle at position
+          val count = grouppedReports.size // count number of different vehicles
+          var avgSpeed = 0
+          grouppedReports foreach( gr => { // first calculate avg speed per each vehicle (possibly multiple reports per segment)
+          val avgSpeedPerVehicle = gr._2.aggregate(0)({ (sum, item) => sum + item(3) }, { (p1, p2) => p1 + p2}) / gr._2.size
+            avgSpeed += avgSpeedPerVehicle // acumulate in the global avg speed
+          })
+          avgSpeed /= count // finally divide per number of different vehicles
+
+          collector.collect(reportsAtPosition._1._1, reportsAtPosition._1._2, reportsAtPosition._1._3, avgSpeed)
+
+        })
+      })(TypeInformation.of(classOf[(Int, Int, Int, Int)]))
+      latestAverageVelocity.print()
 
     // Execute
     env.execute("Linear Road")
