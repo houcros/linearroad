aa97a95a36c7d3ea042f2e7f17ad780470fd9514
Ignacio Lucero <ignacio.lucero@netcentric.biz>
2016-08-25 16:20:32 +0200

    Adds logging support with logback.

--- src/main/scala/de/tu_berlin/dima/bdapro/flink/linearroad/houcros/flink/LinearRoad.scala
+++ src/main/scala/de/tu_berlin/dima/bdapro/flink/linearroad/houcros/flink/LinearRoad.scala
@@ -2,11 +2,15 @@ package de.tu_berlin.dima.bdapro.flink.linearroad.houcros.flink
 
 import java.util.Calendar
 
+import ch.qos.logback.classic.LoggerContext
+import ch.qos.logback.core.util.StatusPrinter
 import es.houcros.linearroad.datasource.CarReportsSource
 import org.apache.flink.api.common.typeinfo.TypeInformation
 import org.apache.flink.streaming.api.TimeCharacteristic
 import org.apache.flink.streaming.api.scala._
 import org.apache.flink.core.fs.FileSystem
+import org.slf4j.LoggerFactory
+
 import scala.collection.mutable
 
 /**
@@ -14,9 +18,13 @@ import scala.collection.mutable
   */
 object LinearRoad {
 
-  var startTime: Long = _
+  final var startTime: Long = _
+  private final val LOG = LoggerFactory.getLogger(LinearRoad.getClass.getSimpleName)
 
   def main(args: Array[String]): Unit = {
+
+    LOG.info("LINEAR ROAD starting!")
+//    StatusPrinter.print(LoggerFactory.getILoggerFactory.asInstanceOf[LoggerContext])
 
     // Set-up
     val env = StreamExecutionEnvironment.getExecutionEnvironment
@@ -27,11 +35,13 @@ object LinearRoad {
     val outputFile = "src/main/resources/output3hours.dat"
 
     // Get custom data source
+    LOG.info("Creating reports data source.")
     val dataStream = new CarReportsSource[Tuple15[Int,Int,Int,Int,Int,Int,Int,Int,Int,Int,Int,Int,Int,Int,Int]](inputFile)
     val reports = env.addSource(dataStream)(TypeInformation.of(classOf[Tuple15[Int,Int,Int,Int,Int,Int,Int,Int,Int,Int,Int,Int,Int,Int,Int]]))
     startTime = Calendar.getInstance.getTimeInMillis
 
     // ** ACCIDENT DETECTION **
+    LOG.info("Creating accident detection and notification handlers.")
     val accidentStream: DataStream[(Int, Int, Int)] = AccidentManager.accidentDetection(reports)
     //accidentStream.print()
     // ** ACCIDENT NOTIFICATION **
@@ -40,10 +50,10 @@ object LinearRoad {
     val accidentNotifications: DataStream[(Int, Int, Int, Int)] = AccidentManager.accidentNotifications(reports, accidentStream)
     // ASK: why wouldn't write with writeAsCsv?
     // Write to file
-    accidentNotifications.writeAsText(outputFile, FileSystem.WriteMode.OVERWRITE).setParallelism(1)
     accidentNotifications.print()
 
     // **SEGMENT STATISTICS**
+    LOG.info("Creating segment statistics calculators.")
     // FIXME: currently gives for this minute, and I want for last minute!
     // Number of vehicles during minute prior to current minute
     val novStream = SegmentStatistics.numberOfVehicles(reports) // (x, d, s, nov)
@@ -69,13 +79,17 @@ object LinearRoad {
     tollNotificationsAndAssessments.print()
 
     // **HQ1: ACCOUNT BALANCE**
+    LOG.info("Creating HQ1 (account balance) handler.")
     val accountBalanceStream = HistoricalQueries.accountBalance(reports, tolls)
     accountBalanceStream.writeAsText(outputFile, FileSystem.WriteMode.OVERWRITE).setParallelism(1)
 
     // **HQ2: DAILY EXPENDITURE**
+    LOG.info("Creating HQ2 (daily expenditure) handler.")
     val dailyExpenditureStream = HistoricalQueries.dailyExpenditure(reports)
+    dailyExpenditureStream.writeAsText(outputFile, FileSystem.WriteMode.OVERWRITE).setParallelism(1)
 
     // Execute
+    LOG.info("Executing the job chain.")
     env.execute("Linear Road")
   }
 
